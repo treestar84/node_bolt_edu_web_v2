53f41b6aaa37c07b5b8ad29b4dc5ae1fd063499d
소나무별 <treestar@sonamubyeol-ui-MacBookPro.local>
2025-07-30 02:10:55 +0900

    port 수정

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
     strictPort: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
       '/server': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => {
